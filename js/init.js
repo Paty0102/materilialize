09c5b49fe2a31151451a1de660ce869f70a322ac
unknown <paty1516_@hotmail.com>
2019-05-13 22:05:20 -0500

    Primer comnit

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+    $(function(){
+
+        $('.sidenav').sidenav();
+        $('.parallax').parallax();
+        $('#textarea1').val('New Text');
+        M.textareaAutoResize($('#textarea1'));
+    });
+})(jQuery);
